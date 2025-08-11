a5aec45bf468179602db7a428af16c6ac5ebd970
rushikeshgokhe <rushikeshgokhe@gmail.com>
2025-08-12 00:35:36 +0530

    Add files via upload

---
+++ area of circlr.py
@@ -0,0 +1,3 @@
+R=int(input("Enter value of radius:"))
+area=3.14*R*R
+print("Area of circle=",area)
---
+++ sec to min to hr.py
@@ -0,0 +1,7 @@
+sec=int(input("Enter value in sec:"))
+minute=sec//60
+rsec=sec%60
+hour=minute//60
+rminute=minute%60
+print("Time=",hour,":",rminute,":",rsec)
+
