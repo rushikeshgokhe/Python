45c72f4d55c5a901a937afa0bc2dc59562266ddb
rushikeshgokhe <rushikeshgokhe@gmail.com>
2025-08-17 00:19:33 +0530

    Add files via upload

---
+++ km to miles.py
@@ -0,0 +1,3 @@
+km=int(input("Enter value in km:"))
+miles=km*0.621371
+print("Value in miles=",miles)
---
+++ negative.py
@@ -0,0 +1,7 @@
+a=int(input("Enter value of a:"))
+if a>0:
+ print("possitive")
+elif a==0:
+ print("zero")
+else:
+ print("Negative")
