e84344c987d0afed3044f1efe3f69749d4f186f2
rushikeshgokhe <rushikeshgokhe@gmail.com>
2025-08-22 08:32:11 +0530

    Add files via upload

---
+++ account balace checker.py
@@ -0,0 +1,25 @@
+name="Rushikesh"
+account=123456789
+balance=500
+print("Enter your choice")
+print("1:deposit:")
+print("2:withdraw:")
+choose=int(input(""))
+if choose==1:
+    depo=int(input("Enter the amt for depo"))
+    currentbalance=balance+depo
+    print("deposit successfuly")
+    print("name=,name")
+    print("acc vale:",account)
+    print("current b:",currentbalance)
+else:
+    wd=int(input("Enter the amt for widthdrw"))
+    currentbalance=balance-wd
+    print("withdraw successfuly")
+    print("name=,name")
+    print("acc vale:",account)
+    print("current b:",currentbalance)
+    
+    
+    
+          
