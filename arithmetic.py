ba8a528ebd3b3fc7368a016b4715d71765c6dfc8
rushikeshgokhe <rushikeshgokhe@gmail.com>
2025-08-30 23:02:51 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,20 @@
+a = int(input("Enter no 1: "))
+b = int(input("Enter no 2: "))
+
+operator = input("Enter any operator: ")
+
+if operator == "+":
+    print("Addition:", a + b)
+elif operator == "-":
+    print("Subtraction:", a - b)
+elif operator == "/":
+    print("Division:", a / b)
+elif operator == "*":
+    print("Multiplication:", a * b)
+else:
+    print("Invalid operator.")
+
+    
+    
+    
+    
