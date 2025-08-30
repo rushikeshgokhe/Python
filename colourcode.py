e7908e5f9f0b380bf6ad1ccca85dfa718f4b53a7
rushikeshgokhe <rushikeshgokhe@gmail.com>
2025-08-30 21:38:45 +0530

    Add files via upload

---
+++ colourcode.py
@@ -0,0 +1,14 @@
+chr=input("Enter any colour code:")
+if chr=="r"  or chr=="R":
+    print("Red")
+elif chr=="w" or chr=="W":
+    print("white")
+elif chr=="y" or chr=="Y":
+    print("yellow")
+elif chr=="b" or chr=="B":
+    print("Blue")
+elif chr=="g" or chr=="G":
+    print("Green")
+else:
+    print("Invalid charecter")
+                
